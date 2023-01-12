449dfad5836087cdcc36f2cd6389a37739ba2b1d
Davis Tumuhaise <tumuhaise@gmail.com>
2023-01-12 08:08:48 +0300

    fix 100

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,12 +1,6 @@
 #!/usr/bin/env bash
 # client config file using puppet
 
-ssh_authorized_key { 'school_key':
-  user => 'ubuntu',
-  type => 'ssh-rsa',
-  key  => file('~/.ssh/school'),
-}
-
-ssh_config { 'PasswordAuthentication':
-  value => 'no',
+exec { 'echo "PasswordAuthentication no\nIdentityFile ~/.ssh/school" >> /etc/ssh/ssh_config':
+        path    => '/bin/'
 }
