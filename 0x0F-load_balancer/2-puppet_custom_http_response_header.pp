b4921fa46a0116b66099b832c7f4aaa036aaa5a4
Davis Tumuhaise <tumuhaise@gmail.com>
2023-01-23 10:57:27 +0300

    fix

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,25 +1,18 @@
 #!/usr/bin/env bash
 # configure new web server and custom headers
 
-sudo apt-get update
-sudo apt-get install nginx -y
-sudo apt-get install puppet -y
-sudo puppet module install puppetlabs-nginx
-sudo apply <<EOF
-class { 'nginx': }
-
-nginx::resource::server { '70942-web-02':
-  listen      => [ '80' ],
-  server_name => '70942-web-02',
-  access_log  => '/var/log/nginx/access.log',
-  error_log   => '/var/log/nginx/error.log',
-  root        => '/var/www/html',
-  location    => {
-    '/' => {
-      'add_header' => [ 'X-Served-By' ],
-    }
-  },
+# class for the custom header
+class custom_http_response_header {
+  package { 'nginx':
+    ensure => installed,
+  }
+  file { '/etc/nginx/conf.d/custom_headers.conf':
+    ensure  => file,
+    content => "add_header X-Served-By ${::hostname};",
+    notify  => Service['nginx'],
+  }
+  service { 'nginx':
+    ensure => running,
+    enable => true,
+  }
 }
-EOF
-
-sudo service nginx restart
