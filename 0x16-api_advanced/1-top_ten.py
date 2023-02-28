16f93fad000c36555468ba18ced4c9edd1a239d9
Davis Tumuhaise <tumuhaise@gmail.com>
2023-03-01 00:32:56 +0300

    1. Top Ten

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+""" writing a function """
+
+
+import requests
+
+
+def top_ten(subreddit):
+    """ function that queries the Reddit
+    API and prints the titles of the
+    first 10 hot posts listed for a
+    given subreddit."""
+
+    endpoint = 'https://www.reddit.com/r/{}/hot.json?limit=10'\
+        .format(subreddit)
+    response = requests.get(endpoint)
+
+    if response.status_code != 200:
+        print(None)
+    else:
+        data = response.json()
+        posts = data['data']['children']
+        return [print(post['data']['title']) for post in posts]
