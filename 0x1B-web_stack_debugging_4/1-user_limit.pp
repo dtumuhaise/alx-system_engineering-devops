17539c2eed3cc711e28ded5bf6ef87b8827af0cc
Davis Tumuhaise <tumuhaise@gmail.com>
2023-03-12 23:49:20 +0300

    fix

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -2,7 +2,7 @@
 
 # Increase hard file limit for holberton user
 exec { 'increase-hard-file-limit-for-holberton-user':
-  command => 'sed -i "/holberton hard/s/5/50000/" /etc/security/limits.conf'
+  command => 'sed -i "/holberton hard/s/4/50000/" /etc/security/limits.conf'
   path    => '/usr/local/bin/:/bin/'
 }
 
