41af2601741d93253d5b3c92956534b15e50fdf2
Davis Tumuhaise <tumuhaise@gmail.com>
2023-05-09 20:07:41 +0300

    fix

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,19 +1,17 @@
 #!/usr/bin/python3
-""" function that queries the reddit API
+""" function that queries the reddit API and
+returns the number of subs of given subreddit
 """
 
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """ function that queries Redit API and
-    returns the number of subs of given subredit"""
-
-    endpoint = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    response = requests.get(endpoint)
-    if response.status_code != 200:
-        return 0
-    else:
-        data = response.json()
-        subs = data['data']['subscribers']
-        return subs
+    """ returns the number of subs of given subreddit
+    """
+    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+    headers = {'User-Agent': 'Mozilla/5.0'}
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 200:
+        return response.json().get('data').get('subscribers')
+    return 0
